637361e41caaedb8ef943dd3fe2db1fc05986693
sfishere <397381741@qq.com>
2023-10-03 22:08:30 +0800

    v0.13版本
    1.hub browse增加过滤，只显示能下载的var包
    2.new to old排序规则修改，按var包创建时间来排序
    3.如果ab包有预览图，则会显示预览
    4.源码删掉，请去新的地址获取源码：https://github.com/sFisherE/var_browser

--- var browser/source/var_browser/hook/uFileBrowser/FileBrowserCallback.cs
+++
@@ -1,4 +0,0 @@
-namespace var_browser
-{
-	public delegate void FileBrowserCallback(string path);
-}
--- var browser/source/var_browser/hook/uFileBrowser/FileBrowserFullCallback.cs
+++
@@ -1,4 +0,0 @@
-namespace var_browser
-{
-	public delegate void FileBrowserFullCallback(string path, bool didClose);
-}
